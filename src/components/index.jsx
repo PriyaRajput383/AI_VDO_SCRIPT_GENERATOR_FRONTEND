8efa1de402be79abb34be75bd6d067575dfad7b0
priya rajput <priyarajput3838@gmail.com>
2026-04-01 20:30:03 +0530

    process

---
+++ src/components/index.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import axios from "axios";
+
+function Index() {
+  const [topic, setTopic] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const generateScript = async () => {
+    if (!topic) return;
+
+    const newMessages = [
+      ...messages,
+      { role: "user", text: topic }
+    ];
+    setMessages(newMessages);
+    setTopic("");
+
+    try {
+      const res = await axios.post("http://localhost:3000/generate-script", {
+        topic,
+      });
+
+      setMessages([
+        ...newMessages,
+        { role: "ai", text: res.data.script }
+      ]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="h-screen flex flex-col bg-black text-white px-20 py-10">
+
+      {/* Header */}
+      <div className="p-4 text-center border-b border-gray-800 text-xl font-semibold ">
+        AI Shorts Script Generator
+      </div>
+
+      {/* Chat Area */}
+      <div className="flex-1 overflow-y-auto p-4 space-y-4">
+        {messages.map((msg, index) => (
+          <div
+            key={index}
+            className={`max-w-xl p-3 rounded-lg ${
+              msg.role === "user"
+                ? "bg-purple-600 ml-auto"
+                : "bg-gray-800 mr-auto"
+            }`}
+          >
+            {msg.text}
+          </div>
+        ))}
+      </div>
+
+      {/* Input */}
+      <div className="p-4 border-t border-gray-800 flex gap-2">
+        <input
+          type="text"
+          value={topic}
+          onChange={(e) => setTopic(e.target.value)}
+          placeholder="Write video topic"
+          className="flex-1 p-3 rounded-lg cursor-pointer bg-gray-900 outline-none"
+        />
+        <button
+          onClick={generateScript}
+          className="bg-purple-600 px-4 rounded-lg"
+        >
+          Send
+        </button>
+      </div>
+
+    </div>
+  );
+}
+
+export default Index;
